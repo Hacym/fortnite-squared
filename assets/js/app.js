10dde343a4ec990d0ac501458d9873397f8f2bd7
MyCah <mycahp@gmail.com>
2018-05-08 15:17:22 -0500

    Add history list

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
+var locationHistory = []
+
 function getLocation() {
     var dropLocation = document.getElementById("location");
+    var historyLocation = document.getElementById("history");
 
     m.request({
         method: "GET",
@@ -7,6 +10,18 @@ function getLocation() {
         extract: function(xhr) { return xhr.responseText }
     }).then(function(newLocation) {
         m.render(dropLocation, m("p", ""));
-        m.render(dropLocation, m("h3", {class: "is-size-2 animated bounce", id:"locationSquare"}, newLocation));
+        locationHistory.unshift(newLocation)
+        m.render(dropLocation, [
+            m("p", "Head to"),
+            m("h3", {class: "is-size-2 animated bounce", id:"locationSquare"}, newLocation),
+            m("p", {class: "has-text-success"}, "Good luck!"),
+        ]);
+        m.render(historyLocation, [
+            m("h3", {class: "is-size-3"}, locationHistory[0]),
+            m("h3", {class: "is-size-3"}, locationHistory[1]),
+            m("h3", {class: "is-size-3", style: "opacity: .6"}, locationHistory[2]),
+            m("h3", {class: "is-size-3", style: "opacity: .3"}, locationHistory[3]),
+            m("h3", {class: "is-size-3", style: "opacity: .1"}, locationHistory[4]),
+        ])
     })
 }
